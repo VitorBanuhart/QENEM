6cac760e7fff843539007464b2484152ee1a1b49
Augusto Mattevi <razanidark@gmail.com>
2025-08-12 19:58:31 -0300

    Aplicando .gitignore e limpando arquivos rastreados

--- qenem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace qenem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
